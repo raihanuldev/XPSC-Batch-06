a8ab26d1a6ed83dea9825ad95879a1255d995b98
raihanuldev <rihanthedev@gmail.com>
2025-05-11 02:21:47 +0600

    bit_manipulation

---
+++ bit manipulation/bitways_operator_and_or.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a =5,b=14;
+    cout<<(a & b)<<endl;   
+    cout<<(a | b)<<endl;
+    return 0;
+}
+
+/**
+ * bitways operation {ms_whiteboard try avaible}
+ * 
+*/
