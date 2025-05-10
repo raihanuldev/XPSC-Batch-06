a0769589712501fbff10fba99749efabf772ad16
raihanuldev <rihanthedev@gmail.com>
2025-05-11 02:42:58 +0600

    power_squer using leftshift operatiore

---
+++ bit manipulation/super_easy_technic_for_power.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int ans = 45;
+    cout<<(2LL<<ans);   
+    return 0;
+}
+
+/**
+ * super power bitways system
+ * */ 
