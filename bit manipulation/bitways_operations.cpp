e75f98aaa5c977bde5f1ca9e94be607d6f6d5136
raihanuldev <rihanthedev@gmail.com>
2025-05-13 06:55:00 +0600

    print_bits_status

--- bit manipulation/bitways_operations.cpp
+++ bit manipulation/bitways_operations.cpp
@@ -5,11 +5,21 @@ bool check_bits_on_or_off(int n, int k)
 {
     return ((n >> k) & 1);
 }
+// prints all bit status
+void print_bits_status(int n){
+    for(int k=7; k>=0;k--){
+        if(check_bits_on_or_off(n,k)){
+            cout<<1<<" ";
+        }else{
+            cout<<0<<" ";
+        }
+    }
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout << check_bits_on_or_off(45, 4) << "\n";
+    print_bits_status(45);
     return 0;
 }
