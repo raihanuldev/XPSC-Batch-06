3878f7e0404405170c1307def0c586d7e0523889
raihanuldev <rihanthedev@gmail.com>
2025-05-13 07:59:44 +0600

    in-built-bitwise-functions

---
+++ bit manipulation/in-built-bitwise-functions.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // how much bits are on
+    // cout<<__builtin_popcount(45);
+    //most greatest bit
+    cout<<__lg(45);
+    return 0;
+}
