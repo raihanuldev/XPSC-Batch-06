cd3f8a508e60531a3d4ad85b6f492f57ed07dcc1
raihanuldev <rihanthedev@gmail.com>
2025-06-23 11:09:09 +0600

    Refactor code structure for improved readability and maintainability

---
+++ Math & Number theory/sieve.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin>>n;
+    vector<bool>prime(n+1,true);
+    for(int i=2; i*i <=n;i++)
+    {
+        if(prime[i]){
+            for(int j=i+i; j<=n;j+=i){
+                prime[j] = false;
+            }
+        }
+    }
+    for(int i=2; i<=n;i++){
+        if(prime[i]){
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
