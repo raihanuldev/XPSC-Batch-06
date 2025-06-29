ee0c462fddf00eb0eb8ea2d827e820ee1bdd3797
raihanuldev <rihanthedev@gmail.com>
2025-06-29 14:23:31 +0600

    Refactor code structure for improved readability and maintainability

---
+++ PBDS/pbds_init.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+// PBDS Template
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+//PBDS init Template
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin>>n;
+    pbds<int>p;
+    for(int i=1; i<=n;i++){
+        int x;
+        cin>>x;
+        p.insert(x);
+    }
+    for(auto val:p){
+        cout<<val<<" ";
+    }
+    return 0;
+}
